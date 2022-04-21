253d4c231d3e5f8f56ef2542794dda1ce603a20c
LGmedia954 <LeslieGonzalez@me.com>
2022-04-21 13:06:57 -0400

    users and organizations controllers

---
+++ app/controllers/api/v1/organizations_controller.rb
@@ -0,0 +1,51 @@
+class Api::V1::OrganizationsController < ApplicationController
+
+  def index
+    @organizations = Organization.all
+
+    render json: @organizations
+  end
+
+  def show
+    organization_json = OrganizationSerializer.new(@organization).serialized_json
+
+    render json: organization_json
+  end
+
+  def create
+    @organization = Organization.new(organization_params)
+
+    if @organization.save
+      session[:organization_id] = @organization.id
+      render json: OrganizationSerializer.new(@organization), status: :created
+    else
+      resp = {
+        error: @organization.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @organization.update(organization_params)
+      render json: @organization
+    else
+      render json: @organization.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @organization.destroy
+  end
+
+  private
+
+    def set_organization
+      @organization = Organization.find(params[:id])
+    end
+
+    def organization_params
+      params.require(:organization).permit(:name, :address, :suite, :city, :state, :zip, :phone, :mission, :website)
+    end
+
+end
---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,52 @@
+class Api::V1::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  def show
+    user_json = UserSerializer.new(@user).serialized_json
+
+    render json: user_json
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      render json: UserSerializer.new(@user), status: :created
+    else
+      resp = {
+        error: @user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:firstname, :lastname, :title, :email, :password)
+    end
+
+  
+end
