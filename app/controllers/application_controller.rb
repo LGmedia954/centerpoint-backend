c649629b2677a1b36d88597925b82ecf232f4955
LGmedia954 <LeslieGonzalez@me.com>
2022-04-22 11:54:00 -0400

    log in fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   include ::ActionController::Cookies
   
    def current_user
-     User.find_by(email: session[:email])
+     User.find_by(id: session[:user_id])
    end
  
    def logged_in?
