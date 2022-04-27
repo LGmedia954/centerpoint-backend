ff4f9df850aa5cb5dcd47eb23e4e801b5781e095
LGmedia954 <LeslieGonzalez@me.com>
2022-04-26 20:15:06 -0400

    announcement serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
   belongs_to :organization
-	# has_many :announcements, through organization
-	# has_many :promos, through organization
-	# has_many :events, through organization
+	has_many :announcements, through: :organization
+	# has_many :promos, through: :organization
+	# has_many :events, through: :organization
 
   validates :firstname, :lastname, :title, presence: true
   validates :email, uniqueness: true
