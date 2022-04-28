17c41a7d04e0fa52856de25ea72b6377a122d646
LGmedia954 <LeslieGonzalez@me.com>
2022-04-28 08:38:57 -0400

    sessions controller

--- app/controllers/api/v1/static_controller.rb
+++ app/controllers/api/v1/static_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::StaticController < ApplicationController
-  before_action :set_user, only: [:about]
+  before_action :get_current_user
 
   def about
-    render "about"
+    if logged_in?
+      render "about"
+    else
+      message: "Please Log In"
+    end
   end
 
 end
