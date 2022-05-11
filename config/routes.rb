2c21f276df2b9e2f13ec31db38c7325acc8751f3
LGmedia954 <LeslieGonzalez@me.com>
2022-05-11 16:17:16 -0400

    route change for org index

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   delete "/api/v1/logout", to: "api/v1/sessions#destroy"
   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
 
-  get "/api/v1/about", to: "api/v1/static#about"
+  get "/api/v1/organizations", to: "api/v1/organizations#index"
   get "/api/v1/mybiz", to: "api/v1/organizations#mybiz"
+  get "/api/v1/about", to: "api/v1/static#about"
   get "/api/v1/contact", to: "api/v1/static#contact"
 
   namespace :api do
