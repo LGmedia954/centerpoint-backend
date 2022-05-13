47cf012b87d2dfef4f7c64652ec29c46aee87232
LGmedia954 <LeslieGonzalez@me.com>
2022-05-13 06:19:58 -0400

    user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,19 +2,5 @@ class UserSerializer
   include JSONAPI::Serializer
   attributes :firstname, :lastname, :title, :email
   has_many :organizations, serializer: OrganizationSerializer
-
-  attribute :organization do |user|
-    {
-      name: user.organization.name,
-      address: user.organization.address,
-      suite: user.organization.suite,
-      city: user.organization.city,
-      state: user.organization.state,
-      zip: user.organization.zip,
-      phone: user.organization.phone,
-      mission: user.organization.mission,
-      website: user.organization.website
-    }
-  end
   
 end
