206990197f1ab48ee6d4eb45a5f909ea6f4386ff
LGmedia954 <LeslieGonzalez@me.com>
2022-05-17 13:26:20 -0400

    dropped 2 routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,10 @@ Rails.application.routes.draw do
   delete "/api/v1/logout", to: "api/v1/sessions#destroy"
   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
 
-  get "/api/v1/organizations", to: "api/v1/organizations#index"
   get "/api/v1/mybiz", to: "api/v1/organizations#mybiz"
   get "/api/v1/about", to: "api/v1/static#about"
   get "/api/v1/contact", to: "api/v1/static#contact"
   get "/api/v1/invite", to: "api/v1/static#invite"
-  get "/api/v1/announcements", to: "api/v1/announcements#index"
 
   namespace :api do
     namespace :v1 do
