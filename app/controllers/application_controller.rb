54628734dc01ebe99e2e884df43f19bf76a4279b
LGmedia954 <LeslieGonzalez@me.com>
2022-05-17 15:20:33 -0400

    routing error troubleshooting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,9 @@ class ApplicationController < ActionController::API
    def logged_in?
      !!current_user
    end
+
+  #  def routing_error(error = 'Routing error', status = :not_found, exception=nil)
+  #    render_exception(404, "Routing Error", exception)
+  #  end
  
  end
